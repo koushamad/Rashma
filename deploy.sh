b9d6129684f0020ceb6d0c3ad2a2f5e72f29b4bb
kousha ghodsizad <kousha.ghodsizad@gmail.com>
2020-06-17 22:12:24 +0300

    fix deploy

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo Please select your deployment state? local, test, live
+read state
+
+if [ $state == "live" ]; then
+  echo Please select your deployment state? up, down, build
+  read state
+
+  if [ $state == "build" ]; then
+    docker rm -f $(docker ps -aq)
+    docker volume rm $(docker volume ls -q)
+    docker-compose up -d --build
+    docker push koushamad/rashma-php-fpm:latest
+    docker push koushamad/rashma-file-server:latest
+    docker koushamad/rashma-socket-server:latest
+
+    docker-compose config > docker-compose-deploy.yaml && kompose convert -f docker-compose-deploy.yaml --out ./docker/k8s
+    docker-compose down
+    rm -rvf docker-compose-deploy.yaml
+  fi
+
+  if [ $state == "up" ]; then
+    kubectl create -f ./docker/k8s --save-config
+  fi
+
+  if [ $state == "down" ]; then
+    kubectl delete -f ./docker/k8s
+  fi
+fi
+
+if [ $state == "test" ]; then
+  docker-compose down
+  docker-compose up -d
+fi
+
+if [ $state == "local" ]; then
+  docker-compose up --build  --remove-orphans
+fi
+
+
+
